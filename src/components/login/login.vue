b65d0f83dad51895fa63c941e393adc02f2610df
JangTaeHee <31396064+JangTaeHee@users.noreply.github.com>
2018-06-02 15:58:54 +0900

    firebase login commit

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,25 +3,43 @@
             <p>Login</p>
         <div class="input-group">
             <label for="user-id">ID</label>
-            <input v-moel="userID" type="text" class="user-id">
+            <input v-model="userID" type="text" class="user-id">
         </div>
         <div class="input-group">
             <label for="user-pw">PW</label>
             <input v-model="userPW" type="password" calss="user-pw">
         </div>
         <div class="btn-group">
-            <router-link to="/weather" class="btn-login">Login</router-link>
+            <button class="btn-login" @click="loginCheck">Login</button>
         </div>
     </div>
 </template>
 
 <script>
+import Router from 'vue-router'
+var router = new Router() ;
+
 export default {
     name : 'Login',
     data(){
         return {
-            userID: '',
-            userPW: '',
+            userID: 'test@test.com ',
+            userPW: '123456',
+        }
+    },
+    methods: {
+        loginCheck(){
+                //event.preventDefault();
+
+                firebase.auth().signInWithEmailAndPassword(this.userID, this.userPW).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // ...
+                })
+                .then((result) => {
+                    this.$router.push('/weather');
+                });
         }
     }
 }
